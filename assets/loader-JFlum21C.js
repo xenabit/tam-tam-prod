0eb51e985b717ca4b28fe11d21cb575ea7a160f0
Xenia <k.nikolaevna98@gmail.com>
2026-01-28 12:33:41 +0300

    fdsfsf

---
+++ assets/loader-JFlum21C.js
@@ -0,0 +1,1 @@
+import{j as e}from"./vendor-Czmxryr-.js";const s="_Loader_1v4kw_2",o="_Loader__spinner_1v4kw_9",r={Loader:s,Loader__spinner:o};function a(){return e.jsx("section",{className:r.Loader,children:e.jsx("div",{className:r.Loader__spinner})})}export{a as L};
--- src/components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Loader.module.scss';
-
-export default function Loader() {
-  return (
-    <section className={styles.Loader}>
-      <div className={styles.Loader__spinner}></div>
-    </section>
-  );
-}
